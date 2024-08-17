1c37313fa4357c860b80bad71d3849d104d1e1ab
Nilay Shah <nilayshah630@gmail.com>
2024-08-17 14:32:48 +0530

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import "./App.css";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-
-    </div>
+    <Router className="flex flex-col min-h-screen overflow-hidden">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,60 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="absolute w-full z-30">
       <div className="container mx-auto">
         <div className="flex items-center justify-between h-20">
-            
+          <div className="flex-shrink-0 mr-4">
+            <Link className="flex flex-row" to="/">
+              <img
+                loading="lazy"
+                className="h-10 fill-current text-purple-600"
+                src={logo}
+                alt="Logo"
+              />
+            </Link>
+          </div>
+
+          <nav className="hidden lg:flex md:flex-grow">
+            <ul className="flex flex-grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  className="text-gray-200 hover:text-gray-100 font-semibold px-4 py-2 flex items-center transition duration-150 ease-in-out"
+                  to="/features"
+                >
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-200 hover:text-gray-100 font-semibold px-4 py-2 flex items-center transition duration-150 ease-in-out"
+                  to="/integrations"
+                >
+                  Integrations
+                </Link>
+              </li>
+            </ul>
+            <ul className="flex flex-grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  className="text-gray-200 font-medium px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                  to="/signin"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <div className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                  <Link to="/signup">Sign up</Link>
+                </div>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="lg:hidden"></div>
         </div>
       </div>
     </header>
