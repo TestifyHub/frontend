cb6a945a3ad9dc350ed58a95d1545f9869dbeecb
Nilay Shah <nilayshah630@gmail.com>
2024-08-17 13:30:22 +0530

    add favicon and svg

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between h-20">
+            
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
